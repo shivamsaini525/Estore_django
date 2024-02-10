f7b8cd7ab396ed059f00dd7d0fcc267da1c9c9f9
shivam <sainishivamsaini12345@gmail.com>
2024-02-10 19:05:36 +0530

    code format  complete

--- store/views.py
+++ store/views.py
@@ -22,11 +22,30 @@ def index(request):
    return render(request,'index.html',data)
 
 
-def signup(request):
-   if request.method == "GET":
-     return render(request,'signup.html')
-   else:
+def validateCustomer(customer):
+   error_message=None
+
+   if(not customer.first_name):
+      error_message="First Name is Required"
+   elif len(customer.first_name) < 3:
+      error_message ="First Name must be 3 Char long or more"
+   elif not customer.last_name:
+      error_message="Last Name is Required"
+   elif not customer.phone:
+      error_message="Phone is Required"
+   elif len(customer.phone) < 10:
+      error_message="Phone Number is Incorrect"
+   elif not customer.email:
+      error_message="Email is Required"
+   elif len(customer.password) < 6:
+      error_message="Password must be 6 char long or more"
+   elif  customer.isExit():
+      error_message="Email Already used"   
+   return error_message 
+
+def registerUser(request):
       postdata=request.POST
+
       first_name=postdata.get('first_name')
       last_name=postdata.get('last_name')
       email=postdata.get('email')
@@ -36,7 +55,8 @@ def signup(request):
       city=postdata.get('city')
       state=postdata.get('state')
       password=postdata.get('password')
-
+      
+      #old values for form
       value={
          'first_name' : first_name,
          'last_name': last_name,
@@ -46,13 +66,7 @@ def signup(request):
          'address':address,
          'city':city,
          'state':state,
-
       }
-
-
-      #validation
-      
-      error_message= None
 
       customer=Customer(
          first_name=first_name,
@@ -66,22 +80,8 @@ def signup(request):
          password=password
       )
 
-      if(not first_name):
-         error_message="First Name is Required"
-      elif len(first_name) < 3:
-         error_message ="First Name must be 3 Char long or more"
-      elif not last_name:
-         error_message="Last Name is Required"
-      elif not phone:
-         error_message="Phone is Required"
-      elif len(phone) < 10:
-         error_message="Phone Number is Incorrect"
-      elif not email:
-         error_message="Email is Required"
-      elif len(password) < 6:
-         error_message="Password must be 6 char long or more"
-      elif  customer.isExit():
-         error_message="Email Already used"         
+      error_message=validateCustomer(customer)  
+
       if not error_message:
          customer.password=make_password(customer.password)
          customer.register()
@@ -90,3 +90,10 @@ def signup(request):
          data= {'error':error_message,
                 'values': value}
          return render(request,'signup.html', data)
+
+
+def signup(request):
+   if request.method == "GET":
+     return render(request,'signup.html')
+   else:
+     return registerUser(request)
